240d9b75347be3ab56b7eeb4c5658a962aea95c2
asalako <augustin-semevo.salako@etudiant.univ-rennes.fr>
2025-01-31 12:01:57 +0100

    Project initialized, and structure created

---
+++ src/main/java/fr/istic/asalako/e_ticket/models/Event.java
@@ -0,0 +1,20 @@
+package fr.istic.asalako.e_ticket.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/test/java/fr/istic/asalako/e_ticket/ETicketApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.istic.asalako.e_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
